1f43b944c4edd786e3f34037b7792aa7af93e8e4
Viraj Khanna <virajkhanna2811@gmail.com>
2021-11-06 10:06:46 +0530

    Add updated files

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -11,11 +11,11 @@ void kmain(void)
     clear_screen();
     print_string("ViniuxOS DOS 1.0 - Made by Viraj Khanna");
     vga_index = 80;
-    print_string("Copyright 2021-Present © The ViniuxOS Developers");
+    print_string("Copyright 2021-Present @ The ViniuxOS Developers");
     vga_index = 240;
     print_string("Hi!");
     vga_index = 320;
-    print_string("Welcome to ViniuxOS-DOS");
+    print_string("Welcome to ViniuxOS-DOS 1.0");
     vga_index = 480;
     print_string("Automatically logged in as root");
     vga_index = 640;
