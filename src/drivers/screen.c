154aec1d3e295bae72998f1a5f9b20ce257f1564
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2022-08-11 12:15:05 +0530

    Add scrolling

--- src/drivers/screen.c
+++ src/drivers/screen.c
@@ -1,15 +1,69 @@
 #define VGA_ADDRESS 0xB8000
-#define BLACK 0
-#define BLUE 1
-#define GREEN 2
-#define RED 4
-#define BROWN 6
-#define GRAY 7
-#define YELLOW 14
-#define WHITE 15
+#define BLACK 0x00
+#define BLUE 0x01
+#define GREEN 0x02
+#define RED 0x04
+#define BROWN 0x06
+#define GRAY 0x07
+#define YELLOW 0x0e
+#define WHITE 0x0f
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
 
 unsigned short *terminal_buffer;
 unsigned int vga_index;
+
+void set_cursor(int offset) {
+    offset /= 2;
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset >> 8));
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+    port_byte_out(VGA_DATA_REGISTER, (unsigned char) (offset & 0xff));
+}
+
+int get_cursor() {
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_HIGH);
+    int offset = port_byte_in(VGA_DATA_REGISTER) << 8;
+    port_byte_out(VGA_CTRL_REGISTER, VGA_OFFSET_LOW);
+    offset += port_byte_in(VGA_DATA_REGISTER);
+    return offset * 2;
+}
+
+void set_char_at_video_memory(char character, int offset) {
+    unsigned char *vidmem = (unsigned char *) VIDEO_ADDRESS;
+    vidmem[offset] = character;
+    vidmem[offset + 1] = WHITE_ON_BLACK;
+}
+
+int get_row_from_offset(int offset) {
+    return offset / (2 * MAX_COLS);
+}
+
+int get_offset(int col, int row) {
+    return 2 * (row * MAX_COLS + col);
+}
+
+void memory_copy(char *source, char *dest, int nbytes) {
+    int i;
+    for (i = 0; i < nbytes; i++) {
+        *(dest + i) = *(source + i);
+    }
+}
+
+int scroll_ln(int offset) {
+    memory_copy(
+            (char *) (get_offset(0, 1) + VIDEO_ADDRESS),
+            (char *) (get_offset(0, 0) + VIDEO_ADDRESS),
+            MAX_COLS * (MAX_ROWS - 1) * 2
+    );
+
+    for (int col = 0; col < MAX_COLS; col++) {
+        set_char_at_video_memory(' ', get_offset(col, MAX_ROWS - 1));
+    }
+
+    return offset - 2 * MAX_COLS;
+}
 
 void print_color_string(char *str, unsigned char color)
 {
