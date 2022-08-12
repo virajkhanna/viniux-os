5fb594eb08beae1228274416e6e1ce72d6eca6da
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2022-08-12 11:54:24 +0530

    Update util.c

--- src/kernel/util.c
+++ src/kernel/util.c
@@ -1,11 +1,12 @@
 #include "util.h"
 
-void memory_copy(char *source, char *dest, int nbytes) {
+void memory_copy(uint8_t *source, uint8_t *dest, uint32_t nbytes) {
     int i;
     for (i = 0; i < nbytes; i++) {
         *(dest + i) = *(source + i);
     }
 }
+
 
 void memory_set(u8 *dest, u8 val, u32 len) {
     u8 *temp = (u8 *)dest;
